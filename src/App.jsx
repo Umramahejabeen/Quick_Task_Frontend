451cbb5db75bd0d1e5c150795cf3a7454fa1186c
Umra Mahejabeen <umramahejabeen1810@gmail.com>
2025-12-30 11:28:45 +0530

    your update message

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,36 @@
+// import { Routes, Route } from "react-router-dom";
+
+// /* Auth Pages */
+// import Login from "./auth/Login";
+// import Register from "./auth/Register";
+// import ForgotPassword from "./auth/ForgotPassword";
+// import ChangePassword from "./auth/ChangePassword";
+// import ResetPassword from "./auth/ResetPassword";
+
+// /* Dashboard */
+// import TaskList from "./tasks/TaskList";
+
+// export default function App() {
+//   return (
+//     <Routes>
+//       {/* DEFAULT */}
+//       <Route path="/" element={<Login />} />
+
+//       {/* AUTH */}
+//       <Route path="/login" element={<Login />} />
+//       <Route path="/register" element={<Register />} />
+//       <Route path="/forgot-password" element={<ForgotPassword />} />
+//       <Route path="/change-password" element={<ChangePassword />} />
+
+//       {/* TASKLIST / DASHBOARD */}
+//       <Route path="/tasks" element={<TaskList />} />
+//     </Routes>
+//   );
+// }
+
+
+
+
 import { Routes, Route } from "react-router-dom";
 
 /* Auth Pages */
@@ -5,6 +38,7 @@ import Login from "./auth/Login";
 import Register from "./auth/Register";
 import ForgotPassword from "./auth/ForgotPassword";
 import ChangePassword from "./auth/ChangePassword";
+import ResetPassword from "./auth/ResetPassword";
 
 /* Dashboard */
 import TaskList from "./tasks/TaskList";
@@ -21,7 +55,11 @@ export default function App() {
       <Route path="/forgot-password" element={<ForgotPassword />} />
       <Route path="/change-password" element={<ChangePassword />} />
 
-      {/* TASKLIST / DASHBOARD */}
+      {/* 🔑 RESET PASSWORD (BOTH WORK) */}
+      <Route path="/reset-password" element={<ResetPassword />} />
+      <Route path="/reset-password/:token" element={<ResetPassword />} />
+
+      {/* DASHBOARD */}
       <Route path="/tasks" element={<TaskList />} />
     </Routes>
   );
